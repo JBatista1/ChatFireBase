bcca8ba50a11840851e5dcb8112b683b58110912
JBATISTA <j.batista.romao@gmail.com>
2019-11-10 14:49:48 -0300

    First Commit

---
+++ ChatFireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatFireBase
+//
+//  Created by João batista Romão on 10/11/19.
+//  Copyright © 2019 João batista Romão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
